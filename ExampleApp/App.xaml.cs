e8b1e132c54b7f36f9f797f297eac175f349e185
Alex Hunt <alexanderthunt@gmail.com>
2022-03-20 20:53:14 -0400

    Made the DockablePanels project a library and made a separate ExampleApp application for launching it.

--- ExampleApp/App.xaml.cs
+++ ExampleApp/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace KFSO.UI.DockablePanels {
+namespace ExampleApp {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
